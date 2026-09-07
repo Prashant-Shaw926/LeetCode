cc5014d69c72fcd6f044e7ae3edff6f1ec3d1c2d
Prashant Kumar Shaw <140432843+Prashant-Shaw926@users.noreply.github.com>
2026-09-07 14:35:11 +0530

    LC-minimum-size-subarray-sum [Runtime: N/A; Memory: N/A]

--- binary-search/minimum-size-subarray-sum.cpp
+++ binary-search/minimum-size-subarray-sum.cpp
@@ -2,22 +2,33 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int n = nums.size();
-        int minLen = INT_MAX, len = 0, sum = 0;
-        int i = 0, j = 0;
+        if (n == 1) {
+            if (nums[0] >= target) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        int minLen = INT_MAX, len = 0;
+        int i = 0, j = i + 1;
+        long long sum = nums[i];
 
-        for (i = 0; i < n; i++) {
+        i = 0;
+        while (j < n) {
             if (nums[i] >= target) {
                 return 1;
             }
-            sum = nums[i];
-            for (j = i + 1; j < n; j++) {
-                sum += nums[j];
-                if (sum >= target) {
-                    len = (j - i) + 1;
-                    if (len < minLen) {
-                        minLen = len;
-                    }
+            sum += nums[j];
+            if (sum >= target) {
+                len = (j - i) + 1;
+                if (len <= minLen) {
+                    minLen = len;
+                    i++;
+                    j = i + 1;
+                    sum = nums[i];
                 }
+            } else {
+                j++;
             }
         }
 
