44dcd5642d1d89f01da2536410509092fbfc4d46
Prashant Kumar Shaw <140432843+Prashant-Shaw926@users.noreply.github.com>
2026-09-13 00:03:36 +0530

    LC-fruit-into-baskets [Runtime: N/A; Memory: N/A]

---
+++ hash-table/fruit-into-baskets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        int i = 0, j = 0, count = 0;
+        int tempA = fruits[i], tempB = fruits[i];
+        int result = INT_MIN;
+
+        for (i = 0; i < n; i++) {
+            count++;
+            tempA = fruits[i];
+            tempB = fruits[i];
+            for (j = i + 1; j < n; j++) {
+                if (tempA != fruits[j] && tempB != fruits[j]) {
+                    count++;
+                    tempB = fruits[j];
+                }
+                if (count > 2) {
+                    count = 0;
+                    break;
+                } else {
+                    int length = j - i + 1;
+                    result = max(result, length);
+                }
+            }
+            count = 0;
+        }
+
+        return result;
+    }
+};
