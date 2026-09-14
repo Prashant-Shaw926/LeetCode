3be622a7075464653d75eeb4e0f5754ba82db39d
Prashant Kumar Shaw <140432843+Prashant-Shaw926@users.noreply.github.com>
2026-09-14 20:51:06 +0530

    LC-fruit-into-baskets [Runtime: N/A; Memory: N/A]

--- array/fruit-into-baskets.cpp
+++ array/fruit-into-baskets.cpp
@@ -2,33 +2,49 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
-        if(n==1){
+        if (n == 1) {
             return 1;
         }
-        int i = 0, j = 0, count = 0;
-        int tempA = fruits[i], tempB = fruits[i];
+        int i = 0, j = 0;
+        int temp;
         int result = INT_MIN;
+        unordered_map<int, int> freq;
 
-        for (i = 0; i < n; i++) {
-            count++;
-            tempA = fruits[i];
-            tempB = fruits[i];
-            for (j = i + 1; j < n; j++) {
-                if (tempA != fruits[j] && tempB != fruits[j]) {
-                    count++;
-                    tempB = fruits[j];
+        while (j < n) {
+            freq[fruits[j]]++;
+            if (freq.size() > 2) {
+                temp = fruits[i];
+                while (freq[temp] != 0) {
+                    freq[temp]--;
+                    i++;
                 }
-                if (count > 2) {
-                    count = 0;
-                    break;
-                } else {
-                    int length = j - i + 1;
-                    result = max(result, length);
-                }
+                freq.erase(temp);
             }
-            count = 0;
+            int length = j - i + 1;
+            result = max(result, length);
+            j++;
         }
 
         return result;
     }
 };
+
+// for (i = 0; i < n; i++) {
+//     count++;
+//     tempA = fruits[i];
+//     tempB = fruits[i];
+//     for (j = i + 1; j < n; j++) {
+//         if (tempA != fruits[j] && tempB != fruits[j]) {
+//             count++;
+//             tempB = fruits[j];
+//         }
+//         if (count > 2) {
+//             count = 0;
+//             break;
+//         } else {
+//             int length = j - i + 1;
+//             result = max(result, length);
+//         }
+//     }
+//     count = 0;
+// }
